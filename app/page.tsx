9a7e5f72fc4603d42219826a90b0f4a953147769
Navkanwal Singh <99899076+navkanwal@users.noreply.github.com>
2024-06-29 15:46:03 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import LandingPage from "@/components/LandingPage"; // Import the LandingPage component
+
+export default function Home() {
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+      <LandingPage />
+    </main>
+  );
+}
